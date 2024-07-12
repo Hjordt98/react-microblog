b2db417c3798f3dd2ef3139c8cd6875287a7ed60
Hjordt98 <andreas.hjordt@gmail.com>
2024-07-12 10:39:49 +0200

    ch_4_creating_page_componenets

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import Container from 'react-bootstrap/Container';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
-import Body from './components/Body';
-import Post from './components/Posts'
-
+import FeedPage from './pages/FeedPage';
+import ExplorePage from './pages/ExplorePage';
+import LoginPage from './pages/LoginPage';
 
 export default function App() {
-  return(
-      <Container fluid className='App'>
-        <Header/>
-        <Body sidebar>
-          <Post/>
-        </Body>
+  return (
+    <Container fluid className="App">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<FeedPage />} />
+          <Route path="/explore" element={<ExplorePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </Container>
-   );
+  );
 }
