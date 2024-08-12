60a18297a40fd0018338cba4ed43e1df460c77fc
Hjordt98 <andreas.hjordt@gmail.com>
2024-08-12 11:07:08 +0200

    ch 5 rendering blog posts

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,28 +1,48 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Spinner from 'react-bootstrap/Spinner'
+
+const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 
 export default function App() {
   const [posts, setPosts] = useState();
 
+  useEffect(() => {
+    (async() =>{ 
+      const response = await fetch(BASE_API_URL + '/api/feed');
+      if (response.ok) {
+        const results = await response.json();
+        setPosts(results.data);
+      } else {
+        setPosts(null);
+      }
+    })();
+  }, []);
+
   return (
     <>
-      { posts === undefined ?
+      {posts === undefined ?
         <Spinner animation="border"/>   
       :  
         <>
-          {posts.length === 0 ?
-            <p>There are no blog posts.</p>
+        {posts === null ?
+          <p>Could not retrieve blog posts</p> 
           :
+          <>
+          {posts.length === 0 ?
+            <p>There are no posts</p>
+          :  
             <>
-              {posts.map(post => {
-                return (
-                  <p key={post.id}>
-                      <b>{post.author.username}</b> &mdash; {post.timestamp}
-                      <br />
-                      {post.text}
-                  </p>
-                );
-              })}
+                    {posts.map(post => {
+                      return (
+                        <p key={post.id}>
+                            <b>{post.author.username}</b> &mdash; {post.timestamp}
+                            <br />
+                            {post.text}
+                        </p>
+                    );
+                   })}
+                   </>
+                }
             </>
           }
       </>
