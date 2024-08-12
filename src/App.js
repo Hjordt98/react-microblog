66606467d1929cd47f7520551596a83ddd346912
Hjordt98 <andreas.hjordt@gmail.com>
2024-08-12 10:41:04 +0200

    ch 5 using state variables

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import FeedPage from './pages/FeedPage';
 import ExplorePage from './pages/ExplorePage';
-import UserPage from './pages/UerPage';
+import UserPage from './pages/UserPage';
 import LoginPage from './pages/LoginPage';
 
 export default function App() {
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,38 +1,32 @@
-export default function App(){
-const posts = [
-    {
-      id: 1,
-      text: 'Hello, World',
-      timestamp: 'a minute a go',
-      author: {
-        username: 'susan',
-      },
-    },
-    {
-      id: 2,
-      text: 'Second Post',
-      timestamp: 'an hour ago',
-      author: {
-        username: 'john',
-      },
-    },
-  ];
+import { useState } from "react";
+import Spinner from 'react-bootstrap/Spinner'
+
+export default function App() {
+  const [posts, setPosts] = useState();
 
   return (
     <>
-      {posts.length === 0 ?
-        <p>There are no blog posts.</p>
-      :
-        posts.map(post => {
-          return (
-            <p key={post.id}>
-              <b>{post.author.username}</b> &mdash; {post.timestamp}
-              <br />
-              {post.text}
-            </p>
-          );
-        })
-      }
-    </>
+      { posts === undefined ?
+        <Spinner animation="border"/>   
+      :  
+        <>
+          {posts.length === 0 ?
+            <p>There are no blog posts.</p>
+          :
+            <>
+              {posts.map(post => {
+                return (
+                  <p key={post.id}>
+                      <b>{post.author.username}</b> &mdash; {post.timestamp}
+                      <br />
+                      {post.text}
+                  </p>
+                );
+              })}
+            </>
+          }
+      </>
+    }
+  </>
   );
 }
